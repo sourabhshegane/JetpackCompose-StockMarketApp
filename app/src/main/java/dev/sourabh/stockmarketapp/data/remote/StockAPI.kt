75e8fc421da8c8598d0507915f32bb859fdf63d3
Sourabh <sourabh.shegane@flytbase.com>
2022-05-15 14:48:29 +0530

    Feat: Add Retrofit API call to alphavantage

---
+++ app/src/main/java/dev/sourabh/stockmarketapp/data/remote/StockAPI.kt
@@ -0,0 +1,18 @@
+package dev.sourabh.stockmarketapp.data.remote
+
+import okhttp3.ResponseBody
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface StockAPI {
+
+    @GET("query?function=LISTING_STATUS")
+    suspend fun getAllListings(
+        @Query("apikey") apiKey: String
+    ): ResponseBody
+
+    companion object{
+        const val API_KEY = "ASE8YNTQ6YLL1YR0"
+        const val BASE_URL = "https://alphavantage.co"
+    }
+}
