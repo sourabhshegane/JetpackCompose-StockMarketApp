d6a4a5ec95a378d34fbb9a5fcc69e634b55fc4cb
Sourabh <sourabh.shegane@flytbase.com>
2022-05-15 17:15:25 +0530

    Refactor: Mark MaiNActivity as Android Entry Point for DI

--- app/src/main/java/dev/sourabh/stockmarketapp/MainActivity.kt
+++ app/src/main/java/dev/sourabh/stockmarketapp/MainActivity.kt
@@ -7,8 +7,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import dagger.hilt.android.AndroidEntryPoint
 import dev.sourabh.stockmarketapp.ui.theme.StockMarketAppTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
