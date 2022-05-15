5a753c6f7be09d50584501d734f1098772871f0f
Sourabh <sourabh.shegane@flytbase.com>
2022-05-15 14:51:31 +0530

    Feat: Add CompanyListingEntity

---
+++ app/src/main/java/dev/sourabh/stockmarketapp/data/local/CompanyListingEntity.kt
@@ -0,0 +1,13 @@
+package dev.sourabh.stockmarketapp.data.local
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class CompanyListingEntity(
+    @PrimaryKey
+    val id: Int? = null,
+    val name: String,
+    val symbol: String,
+    val exchange: String
+)
