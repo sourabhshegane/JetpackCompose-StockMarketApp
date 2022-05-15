b030a3f8f6ecf3d921d3241398502db51e070c93
Sourabh <sourabh.shegane@flytbase.com>
2022-05-15 16:29:29 +0530

    Feat: Implement ViewModel for Company Listings screen

--- app/src/main/java/dev/sourabh/stockmarketapp/presentation/company_listings/CompanyListingsViewModel.kt
+++ app/src/main/java/dev/sourabh/stockmarketapp/presentation/company_listings/CompanyListingsViewModel.kt
@@ -1,8 +1,17 @@
 package dev.sourabh.stockmarketapp.presentation.company_listings
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dev.sourabh.stockmarketapp.domain.repository.StockRepository
+import dev.sourabh.stockmarketapp.util.Resource
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -10,4 +19,47 @@ class CompanyListingsViewModel  @Inject constructor(
     private val stockRepository: StockRepository
 ): ViewModel(){
 
+    var state by mutableStateOf(CompanyListingState())
+    private val searchJob: Job? = null
+
+    fun onEvent(event: CompanyListingEvent){
+        when(event){
+            is CompanyListingEvent.Refresh -> {
+                getCompanyListings(fetchFromRemote = true)
+            }
+            is CompanyListingEvent.OnSearchQueryChange -> {
+                state = state.copy(searchQuery = event.query)
+                searchJob?.cancel()
+                viewModelScope.launch {
+                    delay(500)
+                    getCompanyListings()
+                }
+            }
+        }
+    }
+
+    private fun getCompanyListings(
+        query: String = state.searchQuery.lowercase(),
+        fetchFromRemote:Boolean = false
+    ){
+        viewModelScope.launch {
+            stockRepository.getCompanyListings(fetchFromRemote, query).collect{result ->
+                when(result){
+                    is Resource.Success -> {
+                        result.data?.let { listings ->
+                            state = state.copy(
+                                companies = listings
+                            )
+                        }
+                    }
+                    is Resource.Error -> {
+
+                    }
+                    is Resource.Loading -> {
+                        state = state.copy(isLoading = false)
+                    }
+                }
+            }
+        }
+    }
 }
