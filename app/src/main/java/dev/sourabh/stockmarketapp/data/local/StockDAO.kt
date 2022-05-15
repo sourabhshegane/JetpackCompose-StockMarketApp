dadaff9f8f38acf0af0ec1d560a4d94997297a11
Sourabh <sourabh.shegane@flytbase.com>
2022-05-15 15:05:14 +0530

    Feat: Add Stock DAO

---
+++ app/src/main/java/dev/sourabh/stockmarketapp/data/local/StockDAO.kt
@@ -0,0 +1,20 @@
+package dev.sourabh.stockmarketapp.data.local
+
+import androidx.room.*
+
+@Dao
+interface StockDAO {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertCompanyListings(companyListingEntities: List<CompanyListingEntity>)
+
+    @Query("DELETE FROM companylistingentity")
+    suspend fun clearCompanyListing()
+
+    @Query("""
+        SELECT *
+        FROM companylistingentity
+        WHERE LOWER(name) LIKE '%' || LOWER(:query) ||  '%' OR UPPER(:query) == symbol
+    """)
+    suspend fun searchCompanyListing(query: String): List<CompanyListingEntity>
+}
